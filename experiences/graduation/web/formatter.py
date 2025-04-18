3856916ac09ab547e42b43cc541cd4bee25dfb1c
David Wingate <wingated@cs.byu.edu>
2025-04-18 16:35:23 -0600

    2025 graduates

--- experiences/graduation/web/formatter.py
+++ experiences/graduation/web/formatter.py
@@ -6,10 +6,12 @@ for i in range(len(df)):
     last_name = df.iloc[i]['Last Name']
     first_name = df.iloc[i]['Preferred First Name']
     degree = df.iloc[i]['Degree']
-    major = df.iloc[i]['Major']
+
+    print( f"<div id='name_{i}' class='fader'><div class='text2'>{first_name} {last_name}, {degree}</div></div>" )  # in {major}
+
+#    major = df.iloc[i]['Major']
 #    img = str( df.iloc[i]['Image'] )
 
 #    if img != "" and img != 'nan':
 #        print( f"<div id='name_{i}' class='fader'><div class='text2'>{first_name} {last_name}, {degree}</div><div class='pic' style='background-image:url(\"{img}\")'></div></div>" )  # in {major}
 #    else:
-    print( f"<div id='name_{i}' class='fader'><div class='text2'>{first_name} {last_name}, {degree}</div></div>" )  # in {major}
--- experiences/graduation/web/names.py
+++ experiences/graduation/web/names.py
@@ -5,8 +5,8 @@ df = pd.read_csv( "./data_sorted.csv" )
 for i in range(len(df)):
     last_name = df.iloc[i]['Last Name']
     first_name = df.iloc[i]['Preferred First Name']
-    degree = df.iloc[i]['Degree']
-    major = df.iloc[i]['Major']
+#    degree = df.iloc[i]['Degree']
+#    major = df.iloc[i]['Major']
 #    img = str( df.iloc[i]['Image'] )
 
     print( f"[ {i}, '{first_name} {last_name}' ]," )
