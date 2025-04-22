69883f9ad7707f955374269b9b398cc78011d625
cprice11 <krister.price@gmail.com>
2025-04-21 18:53:03 -0600

    Everything but index

---
+++ experiences/asteroids/controls/lib/AsteroidWatch.tsx
@@ -0,0 +1,44 @@
+import { useMessaging } from "@footron/controls-client";
+import { Box, Button } from "@material-ui/core";
+import { thinWidgetStyle, topUI, fullUIStyle } from "./style";
+import StandardBottomUi from "./standardBottomUi";
+
+export default function AsteroidWatch() {
+  const { sendMessage } = useMessaging();
+
+  const prev = async () => {
+    sendMessage({ type: "watch", value: "previous" });
+  };
+
+  const next = async () => {
+    sendMessage({ type: "watch", value: "next" });
+  };
+
+  return (
+    <div css={fullUIStyle}>
+      <Box css={topUI}>
+        <h3>Select the next close approach</h3>
+      </Box>
+      <StandardBottomUi>
+        <Box css={thinWidgetStyle}>
+          <Button
+            color="primary"
+            variant="contained"
+            size="large"
+            onClick={prev}
+          >
+            <strong>{"<"}</strong>
+          </Button>
+          <Button
+            color="primary"
+            variant="contained"
+            size="large"
+            onClick={next}
+          >
+            <strong>{">"}</strong>
+          </Button>
+        </Box>
+      </StandardBottomUi>
+    </div>
+  );
+}
---
+++ experiences/asteroids/controls/lib/NonStandardDependencies.tsx
@@ -0,0 +1,2 @@
+export { Pagination } from "@mui/material";
+export { Joystick, JoystickShape } from "react-joystick-component";
