3c5180a9ee52479fd7474c43db89335779220ab2
cprice11 <krister.price@gmail.com>
2025-06-25 17:06:46 -0600

    setImage or start

--- experiences/fourier/web/js/main.js
+++ experiences/fourier/web/js/main.js
@@ -47,8 +47,7 @@ if (isAprilFools()) randomChoices.push(rickData);
 function init() {
     initializeOdometer();
 
-    attribution("Almost done")
-
+    
     // Initialize fourier controller
     controller.drawSteps.push(
         { canvas: "leftCanvas", type: "clear" },
@@ -96,14 +95,15 @@ function init() {
             style: { color: palette.fadableOrange, alpha: 1, lineWidth: 2.5 }
         }
     );
-
-    let test = document.getElementById("inProgressTitle")
-    test.style = "color: teal;"
+    
     // To let me play around with things in the console.
     // window.controller = controller;
-
+    
     setImage(null, true);
+    attribution("Almost done")
     controller.start();
+    let test = document.getElementById("inProgressTitle")
+    test.style = "color: teal;"
     // show.startShow()
 
 }
